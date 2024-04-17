3d6bf2a171c5a32dc4d773bbebb143e971847c05
Gabriel Neoob <gabrielngc@gmail.com>
2024-04-17 13:23:36 -0300

    main and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import Container from "./components/container/container.component";
+import Header from "./components/header/header.component";
+import ProductDetails from "./components/product-details/product-details";
+import Product from "./components/product/product.component";
+import ProductModal from "./components/product-modal/product-modal.component";
+// import { useWindowSize } from "./hooks/useWindowSize";
+
 const App = () => {
-  return <div>App</div>;
+  const [openProductModal, setOpenProductModal] = useState(false);
+  // const { windowSize } = useWindowSize();
+
+  return (
+    <div className="">
+      {openProductModal && (
+        <div
+          className="fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center"
+          style={{ backgroundColor: "rgba(0,0,0,0.7)" }}
+        >
+          <ProductModal
+            openProductModal={openProductModal}
+            setOpenProductModal={setOpenProductModal}
+          />
+        </div>
+      )}
+      <Container>
+        <Header />
+        <div className="flex gap-x-4 sm:flex-col sm:items-center">
+          <Product
+            openProductModal={openProductModal}
+            setOpenProductModal={setOpenProductModal}
+          />
+          <ProductDetails />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ModalContextProvider } from "./context/modalContext.tsx";
+import { ProductProvider } from "./context/productContext.tsx";
+import { CartProvider } from "./context/cartContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <ProductProvider>
+        <ModalContextProvider>
+          <App />
+        </ModalContextProvider>
+      </ProductProvider>
+    </CartProvider>
   </React.StrictMode>
 );
