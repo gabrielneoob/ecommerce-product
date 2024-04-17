70766e9a0f56561dda8265719948ed5c6f74378f
Gabriel Neoob <gabrielngc@gmail.com>
2024-04-17 13:23:07 -0300

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25, 100%, 94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+        "black-opacity": "hsl(0,0%,0%",
+      },
+    },
+    screens: {
+      md: { max: "767px" },
+      sm: { max: "850px" },
+    },
   },
   plugins: [],
 };
